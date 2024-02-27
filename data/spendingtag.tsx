53b89acd25e3471c5cecec6433bf5cf4b18a35c1
Phạm Đình Thanh Bình <thanhbinhtk052@gmail.com>
2024-02-27 14:23:58 +0700

    Manage screen DONE

---
+++ data/spendingtag.tsx
@@ -0,0 +1,34 @@
+const SpendingTag = [
+  {
+    label: 'Vay',
+    color: '#9c69b5',
+    value: 10,
+  },
+  {
+    label: 'Thuê nhà',
+    color: '#9d999e',
+    value: 40,
+  },
+  {
+    label: 'Ăn uống',
+    color: '#24b1e0',
+    value: 50,
+  },
+  {
+    label: 'Phí đi lại',
+    color: '#076585',
+    value: 20,
+  },
+  {
+    label: 'Hóa đơn',
+    color: '#ed6fd4',
+    value: 20,
+  },
+  {
+    label: 'Khác',
+    color: '#ed6f82',
+    value: 20,
+  },
+];
+
+export default SpendingTag;
--- screens/manage.screen.tsx
+++ screens/manage.screen.tsx
@@ -1,40 +1,31 @@
 /* eslint-disable react-native/no-inline-styles */
-import React from 'react';
-import {
-  Dimensions,
-  ScrollView,
-  Text,
-  TouchableOpacity,
-  View,
-  processColor,
-} from 'react-native';
-import Animated, {SlideInRight} from 'react-native-reanimated';
-import {LineChart, PieChart} from 'react-native-charts-wrapper';
+import React, {useState} from 'react';
+import {Text, TouchableOpacity, View, processColor} from 'react-native';
+import {PieChart} from 'react-native-charts-wrapper';
+import userSpending from '../data/userspending';
+import SpendingTag from '../data/spendingtag';
+import userIncome from '../data/userincome';
+import IncomeTag from '../data/incometag';
 
 const ManageScreen = () => {
-  const data = {
-    dataSets: [
-      {
-        values: [
-          {value: 50, label: 'Slice 1', maker: '50%'},
-          {value: 10, label: 'Slice 2', maker: '10%'},
-          {value: 40, label: 'Slice 3', maker: '40%'},
-        ],
-        label: 'Pie Chart',
-        config: {
-          sliceSpace: 2,
-          selectionShift: 30,
-          drawValues: true,
-          valueTextSize: 20,
-          colors: [
-            processColor('brown'),
-            processColor('red'),
-            processColor('blue'),
-          ],
-        },
-      },
-    ],
-  };
+  const [data, setData] = useState(userSpending);
+  const [bgColor1, setBgColor1] = useState('transparent');
+  const [bgColor2, setBgColor2] = useState('transparent');
+  const [bgColor3, setBgColor3] = useState('pink');
+  const [bgColor4, setBgColor4] = useState('lightgreen');
+  const [bgColor5, setBgColor5] = useState('transparent');
+  const totalSpending = SpendingTag.map(p => p.value).reduce(
+    (accumulator, element) => {
+      return accumulator + element;
+    },
+    0,
+  );
+  const totalIncome = IncomeTag.map(p => p.value).reduce(
+    (accumulator, element) => {
+      return accumulator + element;
+    },
+    0,
+  );
 
   return (
     <View style={{flex: 1}}>
@@ -45,10 +36,17 @@ const ManageScreen = () => {
           margin: '5%',
         }}>
         <TouchableOpacity
+          onPress={() => {
+            setBgColor4('lightgreen');
+            setBgColor5('transparent');
+            setData(userSpending);
+          }}
           style={{
             flex: 1,
-            backgroundColor: 'lightgreen',
+            backgroundColor: bgColor4,
             justifyContent: 'center',
+            borderWidth: 1,
+            borderColor: '#ccc',
             borderRadius: 10,
           }}>
           <Text
@@ -64,15 +62,23 @@ const ManageScreen = () => {
               textAlign: 'center',
               fontSize: 16,
             }}>
-            123.000
+            {totalSpending}.000
           </Text>
         </TouchableOpacity>
         <TouchableOpacity
+          onPress={() => {
+            setBgColor4('transparent');
+            setBgColor5('lightblue');
+            setData(userIncome);
+          }}
           style={{
             flex: 1,
             borderRadius: 10,
-            backgroundColor: 'lightblue',
+            backgroundColor: bgColor5,
             justifyContent: 'center',
+            borderWidth: 1,
+            borderColor: '#ccc',
+            marginLeft: '2%',
           }}>
           <Text
             style={{
@@ -87,7 +93,7 @@ const ManageScreen = () => {
               textAlign: 'center',
               fontSize: 16,
             }}>
-            123.000
+            {totalIncome}.000
           </Text>
         </TouchableOpacity>
       </View>
@@ -96,54 +102,103 @@ const ManageScreen = () => {
           Wow!! bạn thật là một người biết giữ cân bằng cho cuộc sống
         </Text>
       </View>
-      <PieChart
-        style={{
-          width: '100%',
-          height: '80%',
-          alignSelf: 'center',
-        }}
-        usePercentValues={true}
-        marker={{
-          enabled: true,
-          digits: 1,
-          markerColor: processColor('darkorange'),
-          textColor: processColor('black'),
-          textSize: 15,
-        }}
-        styledCenterText={{
-          text: 'Tháng 3',
-          color: processColor('black'),
-          size: 18,
-        }}
-        holeRadius={40}
-        transparentCircleRadius={0}
-        entryLabelTextSize={15}
-        maxAngle={360}
-        chartDescription={{text: 'Hello World', textSize: 15}}
-        legend={{
-          enabled: true,
-          wordWrapEnabled: false,
-          horizontalAlignment: 'CENTER',
-          form: 'DEFAULT',
-          formSize: 20,
-          orientation: 'VERTICAL',
-          custom: {
-            colors: [processColor('black'), processColor('yellow')],
-            labels: ['Slice 1', 'Slice 2'],
-          },
-        }}
-        data={data}
-
-        // width={200}
-        // height={200}
-        // chartConfig={{
-        //   color: () => 'yellow',
-        // }}
-        // accessor="value"
-        // backgroundColor="transparent"
-        // paddingLeft={15}
-        // absolute
-      />
+      <View style={{flex: 1}}>
+        <PieChart
+          style={{
+            width: '100%',
+            height: '90%',
+          }}
+          animation={{
+            durationX: 500,
+            durationY: 500,
+            easingX: 'EaseInCirc',
+          }}
+          usePercentValues={true}
+          noDataText={'Chưa có báo cáo'}
+          holeRadius={40}
+          transparentCircleRadius={0}
+          entryLabelTextSize={15}
+          chartDescription={{text: ''}}
+          legend={{
+            enabled: true,
+            wordWrapEnabled: false,
+            maxSizePercent: 100,
+            drawInside: false,
+            yEntrySpace: 10,
+            horizontalAlignment: 'RIGHT',
+            verticalAlignment: 'CENTER',
+            orientation: 'VERTICAL',
+            textSize: 15,
+            form: 'SQUARE',
+            formSize: 20,
+            formToTextSpace: 10,
+            custom: {
+              colors: SpendingTag.map(c => processColor(c.color)),
+            },
+          }}
+          data={data}
+        />
+      </View>
+      <View style={{flexDirection: 'row', height: '8%', padding: '2%'}}>
+        <TouchableOpacity
+          onPress={() => {
+            setBgColor1('pink');
+            setBgColor2('transparent');
+            setBgColor3('transparent');
+          }}
+          style={{
+            flex: 1,
+            alignItems: 'center',
+            justifyContent: 'center',
+            borderWidth: 1,
+            borderColor: '#ccc',
+            backgroundColor: bgColor1,
+            borderRadius: 14,
+          }}>
+          <Text style={{fontSize: 18, fontWeight: '600', color: '#000'}}>
+            T12/2023
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => {
+            setBgColor1('transparent');
+            setBgColor2('pink');
+            setBgColor3('transparent');
+          }}
+          style={{
+            flex: 1,
+            alignItems: 'center',
+            justifyContent: 'center',
+            borderWidth: 1,
+            borderColor: '#ccc',
+            borderRadius: 14,
+            backgroundColor: bgColor2,
+            marginHorizontal: 10,
+          }}>
+          <Text style={{fontSize: 18, fontWeight: '600', color: '#000'}}>
+            T1/2024
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => {
+            setBgColor1('transparent');
+            setBgColor2('transparent');
+            setBgColor3('pink');
+          }}
+          style={{
+            flex: 1,
+            alignItems: 'center',
+            justifyContent: 'center',
+            borderWidth: 1,
+            borderColor: '#ccc',
+            borderRadius: 14,
+            backgroundColor: bgColor3,
+          }}>
+          <Text style={{fontSize: 18, fontWeight: '600', color: '#000'}}>
+            T2/2024
+          </Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
