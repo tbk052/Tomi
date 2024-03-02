d5a1b16f576cf767f75800b7f73b99dfb3d327a5
Phạm Đình Thanh Bình <thanhbinhtk052@gmail.com>
2024-03-02 10:00:05 +0700

    transfer DONE

--- screens/transference.screen.tsx
+++ screens/transference.screen.tsx
@@ -1,164 +1,9 @@
 /* eslint-disable react-native/no-inline-styles */
-// /* eslint-disable react-native/no-inline-styles */
-// import React, {useEffect, useState} from 'react';
-// import {Text, TextInput, View} from 'react-native';
-// import DropDownPicker from 'react-native-dropdown-picker';
-// import numeral from 'numeral';
-// import VirtualKeyboard from '../components/virtual-keyboard';
-// import Users from '../data/users';
-
 import React, {useState} from 'react';
 import {Text, View} from 'react-native';
-import {ScrollView, TextInput, TouchableOpacity} from 'react-native';
+import {TextInput, TouchableOpacity} from 'react-native';
 import DropDownPicker from 'react-native-dropdown-picker';
 import Users from '../data/users';
-
-// const TransferenceScreen = () => {
-//   const userData = Users;
-
-//   const [userName, setUserName] = useState('');
-//   const [bankNumberInput, setBankNumberInput] = useState('');
-//   const [virtualKeyState, setVirtualKeyState] = useState(false);
-
-//   const bankNumber = parseInt(bankNumberInput, 10);
-
-//   const getKeyInput = (keyInput: React.SetStateAction<string>) => {
-//     setBankNumberInput(keyInput);
-//   };
-//   const filterBankAccount = (input: any) => {
-//     const foundUser = userData.find(user => user.bankNumber === input);
-//     if (foundUser) {
-//       setUserName(foundUser.fullName);
-//     } else {
-//       setUserName('Vui lòng kiểm tra lại Số Tài Khoản');
-//     }
-//   };
-//   if (bankNumberInput.includes('Xóa')) {
-//     setBankNumberInput(pre => pre.slice(0, -4));
-//   }
-
-//   useEffect(() => {
-//     if (bankNumberInput.length === 13 && value === 'tomi') {
-//       filterBankAccount(bankNumberInput);
-//     } else if (bankNumberInput.length < 13) {
-//       setUserName('');
-//     }
-//   }, [bankNumberInput]);
-
-//   // console.log(numeral(10000).format('000.00'));
-//   console.log(typeof bankNumber);
-//   return (
-//     <View style={{flex: 1}}>
-//       <View
-//         style={{
-//           paddingHorizontal: ' 8%',
-//           paddingTop: ' 10%',
-//         }}>
-//         <DropDownPicker
-//           open={open}
-//           value={value}
-//           items={items}
-//           onSelectItem={() => {
-//             setUserName('');
-//             setVirtualKeyState(true);
-//           }}
-//           setOpen={setOpen}
-//           setValue={setValue}
-//           setItems={setItems}
-//           placeholder="TÊN NGÂN HÀNG"
-//           placeholderStyle={{
-//             fontSize: 20,
-//             color: 'grey',
-//             paddingLeft: 15,
-//           }}
-//           labelStyle={{fontWeight: '500', fontSize: 20, paddingLeft: 10}}
-//         />
-//         <TextInput
-//           placeholder=" SỐ TÀI KHOẢN"
-//           onFocus={() => {
-//             if (value === 'tomi') {
-//               setVirtualKeyState(true);
-//             } else {
-//               setUserName('Vui lòng chọn Ngân hàng');
-//             }
-//           }}
-//           value={bankNumberInput}
-//           maxLength={13}
-//           style={{
-//             marginTop: 15,
-//             paddingLeft: 20,
-//             borderWidth: 1,
-//             borderRadius: 10,
-//             fontSize: 20,
-//             color: 'grey',
-//             backgroundColor: '#fff',
-//             zIndex: -4,
-//           }}
-//         />
-//         {userName !== '' && (
-//           <Text
-//             style={{
-//               paddingTop: 15,
-//               fontSize: 20,
-//               color: '#000',
-//               fontWeight: '500',
-//             }}>
-//             {' '}
-//             {userName}
-//           </Text>
-//         )}
-//       </View>
-//       <View
-//         style={{
-//           paddingHorizontal: ' 8%',
-//         }}>
-//         <TextInput
-//           onFocus={() => {
-//             setVirtualKeyState(true);
-//           }}
-//           onBlur={() => {
-//             setVirtualKeyState(false);
-//           }}
-//           maxLength={13}
-//           showSoftInputOnFocus={false}
-//           style={{
-//             marginTop: 15,
-//             paddingLeft: 20,
-//             borderWidth: 1,
-//             borderRadius: 10,
-//             fontSize: 20,
-//             color: 'grey',
-//             backgroundColor: '#fff',
-//           }}
-//           placeholder={'Nhập số tiền'}
-//         />
-
-//         <TextInput
-//           onFocus={() => {
-//             setVirtualKeyState(false);
-//           }}
-//           style={{
-//             height: 130,
-//             marginTop: 15,
-//             paddingLeft: 20,
-//             borderWidth: 1,
-//             borderRadius: 10,
-//             fontSize: 20,
-//             color: 'grey',
-//             backgroundColor: '#fff',
-//           }}
-//           placeholder={'Lời nhắn'}
-//         />
-//       </View>
-//       {virtualKeyState === true && (
-//         <View style={{}}>
-//           <VirtualKeyboard getKeyInput={getKeyInput} />
-//         </View>
-//       )}
-//     </View>
-//   );
-// };
-// export default TransferenceScreen;
 
 const TransferenceScreen = () => {
   const userData = Users;
@@ -172,8 +17,7 @@ const TransferenceScreen = () => {
   const [amount, setAmount] = useState(0);
   const [note, setNote] = useState('');
 
-  const [accountNote, setAccountNote] = useState();
-  // console.log(transferInfo);
+  const [accountNote, setAccountNote] = useState<string>('');
   return (
     <View style={{margin: 20, borderRadius: 14, backgroundColor: '#c9f0f5'}}>
       <View
@@ -185,7 +29,9 @@ const TransferenceScreen = () => {
           open={open}
           value={bankName}
           items={items}
-          onSelectItem={() => {}}
+          onSelectItem={() => {
+            setAccountNote('');
+          }}
           setOpen={setOpen}
           setValue={setBankName}
           setItems={setItems}
@@ -226,12 +72,29 @@ const TransferenceScreen = () => {
             paddingLeft: 30,
             fontWeight: '500',
           }}
+          maxLength={13}
           placeholder="SỐ TÀI KHOẢN"
           onChangeText={input => {
             setBankAccountNumber(input);
+            if (input.length === 13 && bankName !== null) {
+              const foundUser = userData.find(
+                user => user.bankNumber === input,
+              );
+              if (foundUser) {
+                setAccountNote(foundUser.fullName);
+              } else {
+                setAccountNote('Vui lòng kiểm tra lại Số Tài Khoản');
+              }
+            }
+            if (input.length !== 13) {
+              setAccountNote('');
+            }
+            if (bankName === null) {
+              setAccountNote('Vui lòng chọn ngân hàng');
+            }
           }}
         />
-        {accountNote != null && (
+        {accountNote !== '' && (
           <Text style={{marginBottom: 20, fontSize: 18, paddingLeft: 20}}>
             {accountNote}
           </Text>
